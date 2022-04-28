2a2ffd374c209728236f9492777852472d98239d
BlueBeret <63086194+BlueBeret@users.noreply.github.com>
2022-04-29 05:42:29 +0700

    give every page a better title

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,15 +2,24 @@ import '../styles/globals.css'
 import Navbar from '@/components/Navbar/Navbar'
 import { useRouter, Router } from 'next/router'
 import { useEffect } from 'react'
-import NProgress from 'nprogress'; 
-import 'nprogress/nprogress.css'; 
-
+import NProgress from 'nprogress';
+import 'nprogress/nprogress.css';
+import Head from 'next/head'
+import { useState } from 'react'
 Router.events.on('routeChangeStart', () => NProgress.start()); Router.events.on('routeChangeComplete', () => NProgress.done()); Router.events.on('routeChangeError', () => NProgress.done());
 
 
 function MyApp({ Component, pageProps }) {
 
   const router = useRouter()
+
+  const document_title = {
+    '/': "Lurifos - Programmer",
+    '/diary': 'Diary',
+    '/askme': 'Ask Me',
+    '/contact': 'Contact'
+  }
+
   useEffect(() => {
     const bodybg = {
       '/': 'bg-grad-lblue',
@@ -22,6 +31,11 @@ function MyApp({ Component, pageProps }) {
   }, [router.pathname])
 
   return <>
+    <Head>
+      <title>{document_title[router.pathname]}</title>
+      <meta name="description" content="Hello there, I am Lurifos and welcome to my playground. I'm a computer science student who love math." />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
     <Navbar />
     <div className='flex flex-col justify-center'>
       <Component {...pageProps} className="h-full" />
