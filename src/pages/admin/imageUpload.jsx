3c277686413844034f4a4646280cb25e2ce6c925
BlueBeret <63086194+BlueBeret@users.noreply.github.com>
2022-05-01 14:21:14 +0700

    add image upload boiler plate

---
+++ src/pages/admin/imageUpload.jsx
@@ -0,0 +1,92 @@
+import { useState } from 'react';
+import Head from 'next/head'
+
+export default function Home() {
+    const [imageSrc, setImageSrc] = useState();
+    const [uploadData, setUploadData] = useState();
+
+    /**
+     * handleOnChange
+     * @description Triggers when the file input changes (ex: when a file is selected)
+     */
+
+    function handleOnChange(changeEvent) {
+        const reader = new FileReader();
+
+        reader.onload = function (onLoadEvent) {
+            setImageSrc(onLoadEvent.target.result);
+            setUploadData(undefined);
+        }
+
+        reader.readAsDataURL(changeEvent.target.files[0]);
+    }
+
+    /**
+     * handleOnSubmit
+     * @description Triggers when the main form is submitted
+     */
+
+    async function handleOnSubmit(event) {
+        event.preventDefault();
+        const form = event.currentTarget;
+        const fileInput = Array.from(form.elements).find(({ name }) => name === 'file');
+        console.log('fileInput', fileInput);
+        console.log('fileInput.files', fileInput.files);
+
+        const formData = new FormData();
+
+        for (const file of fileInput.files) {
+            formData.append('file', file);
+        }
+        formData.append('upload_preset', 'lurifos.dev');
+        console.log(`https://api.cloudinary.com/v1_1/${process.env.CLOUDNAME}/image/upload`)
+        const data = await fetch(url, {
+            method: 'POST',
+            body: formData
+        }).then(r => r.json());
+
+        console.log(data)
+    }
+
+
+    return (
+        <div >
+            <Head>
+                <title>Image Uploader</title>
+                <meta name="description" content="Upload your image to Cloudinary!" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+            <main >
+                <h1 >
+                    Image Uploader
+                </h1>
+
+                <p >
+                    Upload your image to Cloudinary!
+                </p>
+
+                <form method="post" onChange={handleOnChange} onSubmit={handleOnSubmit}>
+                    <p>
+                        <input type="file" name="file" />
+                    </p>
+
+
+                    {imageSrc && !uploadData && (
+                        <p>
+                            <button>Upload Files</button>
+                        </p>
+                    )}
+
+                    {uploadData && (
+                        <code><pre>{JSON.stringify(uploadData, null, 2)}</pre></code>
+                    )}
+                </form>
+            </main>
+
+            <footer>
+                <p>Find the tutorial on <a href="https://spacejelly.dev/">spacejelly.dev</a>!</p>
+            </footer>
+        </div>
+    )
+}
--- src/pages/admin/index.jsx
+++ src/pages/admin/index.jsx
@@ -12,7 +12,7 @@ const AdminPage = () => {
         const res = await fetch('/api/diary/adddiary', {
             method: 'POST',
             body: JSON.stringify({
-                dtitle : title,
+                dtitle: title,
                 dbody: body,
                 secret: secret
             }),
@@ -20,7 +20,7 @@ const AdminPage = () => {
                 'Content-Type': 'application/json',
             }
         })
-        if(res.status === 200) {
+        if (res.status === 200) {
             setTitle('')
             setBody('')
         }
@@ -29,13 +29,13 @@ const AdminPage = () => {
         <div className="bg-black text-black w-min p-5">
             <form>
 
-            
-            <input type="text" name="title" placeholder="title" value={title} onChange={e => setTitle(e.target.value)} />
-            <input type="password" name="secret" placeholder='secret' value={secret} onChange={e => setSecret(e.target.value)} />
+
+                <input type="text" name="title" placeholder="title" value={title} onChange={e => setTitle(e.target.value)} />
+                <input type="password" name="secret" placeholder='secret' value={secret} onChange={e => setSecret(e.target.value)} />
                 <textarea name="body" placeholder="body" className="h-[200px]" value={body} onChange={e => setBody(e.target.value)} />
-                </form>
-                <br></br>
-            <button onClick={ handleClick} className="bg-white">Submit</button>
+            </form>
+            <br></br>
+            <button onClick={handleClick} className="bg-white">Submit</button>
         </div>
     );
 }
