5414a73ad2d784d433d4280bb9bb249deec14796
BlueBeret <63086194+BlueBeret@users.noreply.github.com>
2022-05-05 08:56:38 +0700

    add trycatch in getlatestdiary

--- src/helpers/getLatestDiary.js
+++ src/helpers/getLatestDiary.js
@@ -2,14 +2,22 @@ import { PrismaClient } from "@prisma/client"
 
 export default async function getLatestDiary() {
     const prisma = new PrismaClient()
-    const data = await prisma.$queryRaw`SELECT * FROM diary ORDER BY timecreated DESC LIMIT 20`
+    try {
+        const data = await prisma.$queryRaw`SELECT * FROM diary ORDER BY timecreated DESC LIMIT 20`
 
-    data.map(x => {
-        x.timecreated = x.timecreated.toString()
-        x.lastedited = x.lastedited.toString()
-        return x
-    })
+        data.map(x => {
+            x.timecreated = x.timecreated.toString()
+            x.lastedited = x.lastedited.toString()
+            return x
+        })
+        await prisma.$disconnect()
+        return data
+    } catch (error) {
+        await prisma.$disconnect()
+        return []
+    }
 
-    await prisma.$disconnect()
-    return data
+
+
+
 }
